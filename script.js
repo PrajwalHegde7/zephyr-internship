bdd21a32a32871a508d9b5010309e51a449d6cec
Prajwal Hegde <prajwalsh234@gmail.com>
2023-08-20 22:41:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function sum(arr) {
+    var sum = 0;
+    arr.forEach((element) => (sum += element));
+    return sum;
+}
+console.log(sum([1, 2, 3, 4, 5, 6]));
+
+function findMaxValue(numbers) {
+    // Your code here
+    var max = numbers[0];
+    numbers.forEach((num) => (max < num ? (max = num) : null));
+    return max;
+}
+// Example usage
+console.log(findMaxValue([5, 9, 2, 6, 3])); // Output: 9
+console.log(findMaxValue([-10, -5, -2, -8])); // Output: -2
+console.log(findMaxValue([100, 87, 92, 105])); // Output: 105
+
+function countEvenNumbers(numbers) {
+    // Your code here
+    var count = 0;
+    numbers.forEach((num) => (num % 2 == 0 ? count++ : null));
+    return count;
+}
+// Example usage
+console.log(countEvenNumbers([1, 2, 3, 4, 5, 6])); // Output: 3
+console.log(countEvenNumbers([10, 21, 32, 45])); // Output: 2
+console.log(countEvenNumbers([7, 9, 11, 13])); // Output: 0
